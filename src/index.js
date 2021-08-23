5b96ae28afdfafbe7e341b4c488e36c8851c2bab
Romakha <55205695+Romakha@users.noreply.github.com>
2021-08-23 19:44:29 +0600

    feat: finish

--- src/index.js
+++ src/index.js
@@ -38,7 +38,33 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    let result = ''
+    for(let i = 0; i < expr.length; i += 10){
+        let letterCode = expr.substring(i, i+10)
+        
+        if(letterCode != '**********'){
+          let morzeKey = ''
+          for(let j = 0; j < letterCode.length; j += 2){
+            let morzeItem = letterCode.substring(j, j+2)
+            //console.log(morzeItem)
+            if(morzeItem === '10'){
+              morzeKey+='.'
+            }else if(morzeItem === '11'){
+              morzeKey+='-'
+            }
+          }
+          let a = MORSE_TABLE[morzeKey]
+          result+=a
+          //console.log(a, morzeKey)
+          morzeKey=''
+
+        }else{
+            result+=' '
+        }
+        
+
+    }
+    return result;
 }
 
 module.exports = {
